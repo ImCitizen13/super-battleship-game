0b668ea8f9c30d6853c643cd9e41cc3dfbd17960
ImCitizen13 <meltohamy@protonmail.com>
2023-12-13 20:25:08 +0400

    ''''

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,26 @@
 import { type AppType } from "next/app";
+import Head from "next/head";
 
 import { api } from "~/utils/api";
 
 import "~/styles/globals.css";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Dhurjati&display=swap"
+          rel="stylesheet"
+        />
+
+        <title>BattleShip Game</title>
+        <meta name="description" content="This game is for the super hackathon" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 };
 
 export default api.withTRPC(MyApp);
